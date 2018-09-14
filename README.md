1b9220f5a6888ad5bc5ccc555a13527adb7787c8
polubis <polubis@wp.pl>
2018-09-14 13:40:34 +0200

    Adding new things

--- README.md
+++ README.md
@@ -1,7 +1,7 @@
 Project libs list: (project was ejected)
 npm install sass-loader node-sass --save-dev, 
 npm run eject
-
+npm install webfontloader --save
 
 
 Technology stack:
--- src/index.js
+++ src/index.js
@@ -2,6 +2,15 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
+import WebFont from 'webfontloader';
+import './index.scss';
+import 'font-awesome/css/font-awesome.min.css';
+
+WebFont.load({
+    google: {
+      families: ['Roboto:300,400,700', 'sans-serif']
+    }
+});
 
 ReactDOM.render(<App />, document.getElementById('root'));
 registerServiceWorker();
