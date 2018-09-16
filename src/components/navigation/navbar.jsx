8117eb477e18f5582a5399eefe0b4a406194cc6b
polubis <polubis@wp.pl>
2018-09-16 22:40:10 +0200

    Changes

--- src/components/navigation/navbar.jsx
+++ src/components/navigation/navbar.jsx
@@ -1,32 +1,50 @@
-import React from 'react'
-import './navbar.scss';
-import { MotiveContext } from '../../services/contextAPI/context';
-import NavBackground from '../../assets/home_nav_background.jpg';
-import Button from '../common/button/button';
-import Link from '../common/link/link';
+import React from "react";
+import "./navbar.scss";
+import { MotiveContext } from "../../services/contextAPI/context";
+import NavBackground from "../../assets/home_nav_background.jpg";
+import Button from "../common/button/button";
+import Link from "../common/link/link";
 
 const navbar = () => {
-    return (
-        <MotiveContext.Consumer>
-            {motive => (
-                <nav id="navbar">
-                    <div className="btns">
-                        <Link path="/" icon="fa-home" color={motive.backgroundColor} />
-                        <section>
-                            <Link path="/about" title="O nas" color={motive.backgroundColor}/>
-                            <Link path="/contact" title="Kontakt" color={motive.backgroundColor}/>
-                            <Link path="/register" title="Rejestracja" color={motive.backgroundColor}/>
-                            <Link path="/login" title="Logowanie" color={motive.backgroundColor}/>
-                        </section>
-                    </div>
-                    <figure className="shadow">
-                        <div style={{backgroundImage: `url(${NavBackground})`}}>
-                        </div>
-                    </figure>
-                </nav>
-            )}
-        </MotiveContext.Consumer>
-    );
-}
+  const isHomePage = window.location.href.endsWith("/") ? true : false;
+  return (
+    <MotiveContext.Consumer>
+      {motive => (
+        <nav id="navbar">
+          <div className="btns">
+            <Link path="/" icon="fa-home" color={motive.backgroundColor} />
+            <section>
+              <Link
+                path="/about"
+                title="O nas"
+                color={motive.backgroundColor}
+              />
+              <Link
+                path="/contact"
+                title="Kontakt"
+                color={motive.backgroundColor}
+              />
+              <Link
+                path="/register"
+                title="Rejestracja"
+                color={motive.backgroundColor}
+              />
+              <Link
+                path="/login"
+                title="Logowanie"
+                color={motive.backgroundColor}
+              />
+            </section>
+          </div>
+          {isHomePage && (
+            <figure className="shadow">
+              <div style={{ backgroundImage: `url(${NavBackground})` }} />
+            </figure>
+          )}
+        </nav>
+      )}
+    </MotiveContext.Consumer>
+  );
+};
 
 export default navbar;
